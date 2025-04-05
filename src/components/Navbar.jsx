a6a6c88104db670593c8e1fc52e35a5bb8de1f9f
Malay Patra <107231583+malay77patra@users.noreply.github.com>
2025-04-05 22:04:14 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 // import { FaBarsStaggered } from "react-icons/fa6";
 import CustomIcon from "./CustomIcon";
-import Logo from '@/assets/logo.svg?react'
+import Logo from '@/assets/logo.svg?react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
+    const navigate = useNavigate();
+    
     return (
         <div className="navbar bg-base-100 shadow-sm">
             {/* <div className="flex-none">
@@ -15,7 +18,7 @@ export default function Navbar() {
                 <a className="p-2 text-xl text-primary" href="/">Flamecoders</a>
             </div>
             <div className="flex-none">
-                <button className="btn btn-primary">Get Started</button>
+                <button className="btn btn-primary" onclick={() => navigate("/auth")}>Get Started</button>
             </div>
         </div>
     )
