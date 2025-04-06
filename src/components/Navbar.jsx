fc34d27e755654f5e7bae96fb9ed17f26e2884c1
malay77patra <malay77patra@gmail.com>
2025-04-06 12:52:00 +0530

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 // import { FaBarsStaggered } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 import CustomIcon from "./CustomIcon";
-import Logo from '@/assets/logo.svg?react'
+import Logo from '@/assets/logo.svg?react';
+import { useAuth } from "@/hooks/useAuth";
 
 export default function Navbar() {
+    const navigate = useNavigate();
+    const { isAuthenticated } = useAuth();
+
     return (
         <div className="navbar bg-base-100 shadow-sm">
             <div className="flex-1 flex items-center">
@@ -11,7 +15,11 @@ export default function Navbar() {
                 <a className="p-2 text-xl text-primary" href="/">Flamecoders</a>
             </div>
             <div className="flex-none">
-                <button className="btn btn-primary">Get Started</button>
+                {isAuthenticated ? (
+                    <>user</>
+                ) : (
+                    <button className="btn btn-primary" onClick={() => navigate("/auth")}>Get Started</button>
+                )}
             </div>
         </div>
     )
