d4672c6f2decb14ca5cc8beb206f8ab93a75cd0d
malay77patra <malay77patra@gmail.com>
2025-04-28 14:28:22 +0530

    trasnition added

--- src/components/ui/Sidebar.jsx
+++ src/components/ui/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useEffect, useRef, useState } from 'react'
 import { FiSidebar } from "react-icons/fi"
 import { useIsMobile } from '@/hooks/use-mobile'
 
@@ -29,16 +29,34 @@ export function SidebarTrigger({ iconSize = "1.25rem" }) {
 }
 
 export function Sidebar({ children }) {
-    const { isOpen } = useContext(SidebarContext)
+    const { isOpen, toggleSidebar } = useContext(SidebarContext)
     const isMobile = useIsMobile();
+    const [showBackdrop, setShowBackdrop] = useState(false)
+    const timeoutRef = useRef(null)
+
+    useEffect(() => {
+        if (!(isOpen && isMobile)) {
+            timeoutRef.current = setTimeout(() => {
+                setShowBackdrop(isOpen && isMobile)
+            }, 300);
+        } else {
+            setShowBackdrop(isOpen && isMobile)
+        }
+
+        return () => {
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current)
+            }
+        }
+    }, [isOpen, isMobile])
 
     return (
         <>
-            {(isMobile && isOpen) && (
-                <div className="bg-black/10 h-screen w-screen fixed top-0 left-0 z-99 flex"></div>
+            {showBackdrop && (
+                <div className={`h-screen w-screen fixed top-0 left-0 z-99 flex transition-all duration-300 ${(isMobile && isOpen) ? 'bg-black/80' : 'bg-black/0'}`} onClick={toggleSidebar}></div>
             )}
             <div className={`${isMobile ? 'fixed left-0 top-0 z-100' : ''} shrink-0 h-screen bg-base-100 overflow-x-hidden transition-all duration-300 border-r border-base-content/20 flex justify-end ${isOpen ? (isMobile ? 'w-76' : 'w-64') : 'w-0'}`}>
-                <div className='w-64 shrink-0'>
+                <div className='w-64 shrink-0 p-2 box-border'>
                     {children}
                 </div>
                 {isMobile && (
